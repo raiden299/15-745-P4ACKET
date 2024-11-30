d93668ced6b1281363510ed7a78a18be6be3e89e
Darshil Kaneria <darshilkaneria@gmail.com>
2024-11-30 02:46:30 -0500

    Add different types of packets to sned

--- send.py
+++ send.py
@@ -2,7 +2,22 @@ import socket
 import sys
 import time
 
-from scapy.all import IP, Ether, get_if_hwaddr, get_if_list, sendp
+from scapy.all import IP, Ether, UDP, get_if_hwaddr, get_if_list, sendp
+from scapy.packet import Packet, bind_layers
+from scapy.fields import ByteField, ShortField, IntField, StrFixedLenField
+
+
+class QUIC(Packet):
+    name = "QUIC"
+    fields_desc = [
+        ByteField("type", 0),
+        ShortField("version", 1),
+        IntField("length", 0),
+        StrFixedLenField("data", b"", length=8)
+    ]
+
+
+bind_layers(UDP, QUIC)
 
 
 def get_if():
@@ -18,6 +33,24 @@ def get_if():
     return iface
 
 
+def create_ipv4_packet(dst_addr):
+    pkt = Ether(src=get_if_hwaddr(get_if()), dst='ff:ff:ff:ff:ff:ff')
+    pkt = pkt / IP(dst=dst_addr)
+    return pkt
+
+
+def create_udp_packet(dst_addr, dst_port):
+    pkt = Ether(src=get_if_hwaddr(get_if()), dst='ff:ff:ff:ff:ff:ff')
+    pkt = pkt / IP(dst=dst_addr) / UDP(dport=dst_port)
+    return pkt
+
+
+def create_quic_packet(dst_addr, dst_port):
+    pkt = Ether(src=get_if_hwaddr(get_if()), dst='ff:ff:ff:ff:ff:ff')
+    pkt = pkt / IP(dst=dst_addr) / UDP(dport=dst_port) / QUIC(data="NeverGonnaGiveYouUp")
+    return pkt
+
+
 def main():
 
     if len(sys.argv) < 2:
@@ -28,14 +61,16 @@ def main():
     iface = get_if()
 
     print("sending on interface %s to %s" % (iface, str(addr)))
-    pkt = Ether(src=get_if_hwaddr(iface), dst='ff:ff:ff:ff:ff:ff')
-    pkt = pkt / IP(dst=addr)
 
-    # Send packets for 30 seconds
+    # pkt = create_ipv4_packet(addr)
+    # pkt = create_udp_packet(addr, 12345)
+    pkt = create_quic_packet(addr, 12345)
+
+    # Send packets at 10Mbps for 30 seconds
     start_time = time.time()
     end_time = start_time + 30
     pkt_size = len(pkt) * 8
-    interval = pkt_size / (10 * 1024 * 1024)  # Interval for 10mbps
+    interval = pkt_size / (10 * 1024 * 1024)
 
     while time.time() < end_time:
         sendp(pkt, iface=iface, verbose=False)
