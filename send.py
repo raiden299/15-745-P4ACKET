46d7f33dbb140449e13ea4ea71cc7fd09005fd31
Darshil Kaneria <darshilkaneria@gmail.com>
2024-11-30 02:26:52 -0500

    update send to allow for a sending rate

--- send.py
+++ send.py
@@ -1,26 +1,26 @@
-#!/usr/bin/env python3
-import random
 import socket
 import sys
+import time
 
-from scapy.all import IP, TCP, Ether, get_if_hwaddr, get_if_list, sendp
+from scapy.all import IP, Ether, get_if_hwaddr, get_if_list, sendp
 
 
 def get_if():
-    ifs=get_if_list()
-    iface=None # "h1-eth0"
+    ifs = get_if_list()
+    iface = None
     for i in get_if_list():
         if "eth0" in i:
-            iface=i
-            break;
+            iface = i
+            break
     if not iface:
         print("Cannot find eth0 interface")
         exit(1)
     return iface
 
+
 def main():
 
-    if len(sys.argv)<2:
+    if len(sys.argv) < 2:
         print('pass 2 arguments: <destination> "<message>"')
         exit(1)
 
@@ -28,10 +28,18 @@ def main():
     iface = get_if()
 
     print("sending on interface %s to %s" % (iface, str(addr)))
-    pkt =  Ether(src=get_if_hwaddr(iface), dst='ff:ff:ff:ff:ff:ff')
-    pkt = pkt /IP(dst=addr)
-    pkt.show2()
-    sendp(pkt, iface=iface, verbose=False)
+    pkt = Ether(src=get_if_hwaddr(iface), dst='ff:ff:ff:ff:ff:ff')
+    pkt = pkt / IP(dst=addr)
+
+    # Send packets for 30 seconds
+    start_time = time.time()
+    end_time = start_time + 30
+    pkt_size = len(pkt) * 8
+    interval = pkt_size / (10 * 1024 * 1024)  # Interval for 10mbps
+
+    while time.time() < end_time:
+        sendp(pkt, iface=iface, verbose=False)
+        time.sleep(interval)
 
 
 if __name__ == '__main__':
