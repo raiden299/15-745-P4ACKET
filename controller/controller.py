6eb405cf4d370bd35457c68389111ff420b23e91
Darshil Kaneria <darshilkaneria@gmail.com>
2024-11-30 01:53:41 -0500

    Add ethernet and ipv4 parsing

--- controller/controller.py
+++ controller/controller.py
@@ -1,4 +1,6 @@
-import p4runtime-shell.p4runtime_shell as sh
+import p4runtime_sh.shell as sh
+import threading
+from scapy.all import Ether, IP
 
 sh.setup(
     device_id=0,
@@ -7,5 +9,22 @@ sh.setup(
     config=sh.FwdPipeConfig('../../prog/simple_test_with_ctrl/p4info.txt', '../../prog/simple_test_with_ctrl/switch_config.json') # Update this when switching experiments
 )
 
-for msg in sh.PacketIn().sniff():
-    print(msg)
+def print_packet(packet):
+    eth = Ether(packet.packet.payload)
+    if eth.haslayer(IP):
+        ip = eth[IP]
+        print(f"Ethernet: {eth.summary()}")
+        print(f"IP: {ip.summary()}")
+    else:
+        print(f"Ethernet: {eth.summary()}")
+
+def start_sniffer():
+    packet_in = sh.PacketIn()
+    try:
+        while True:
+            packet_in.sniff(function=print_packet, timeout=1)
+    except KeyboardInterrupt:
+        print("Bye bye")
+
+sniffer_thread = threading.Thread(target=start_sniffer)
+sniffer_thread.start()
